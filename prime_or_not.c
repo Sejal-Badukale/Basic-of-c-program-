560d64c72d995dbcf97ee9046b545dde13fe305e
Sejal-Badukale <sejalbadukale@gmail.com>
2025-01-10 21:31:55 +0530

    Add files via upload

---
+++ prime_or_not.c
@@ -0,0 +1,25 @@
+//2.write a c program to find number is prime or not.
+#include <stdio.h>
+int main() {
+    int num, i, flag = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+        for (i = 2; i * i <= num; i++) 
+		{
+            if (num % i == 0)
+			 {
+                flag = 1;
+                break;
+            }
+        }
+        if (flag == 1) 
+		{
+            printf("%d is not a prime number.\n", num);
+        } else 
+		{
+            printf("%d is a prime number.\n", num);
+        }
+    }
+
+
+
